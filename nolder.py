76172de62463003a13b39e58be5f72b1e03d8b8c
daveomri <daom9999@gmail.com>
2018-06-27 23:46:11 +0200

    new function that is searching for empty files, but it's slow. So I've to change it..

--- nolder.py
+++ nolder.py
@@ -1,48 +1,94 @@
 #!/usr/bin/env python3
 # David Omrai, 20.6.2018
 # Program for searching empty folders
-import os
+import os, time
 class Nolder:
     def __init__(self):
+        self.timeProgram = time.time()
         self.visitedFolders = ()
         #self.visitedFolders = list()
-        self.mainFolder = self.findDir()
-        del self.beBack
-        self.homeDir = os.path.abspath(".")
-        self.remoteDir = list()
-        self.emptyDirs = open("emptyDirs.txt", "a+")
+        self.homeDir = os.getcwd()
+        self.remoteCloud = ()
         self.emptyFilesFinder()
     def findDir(self):
-        self.beBack = list()
+        beBack = list()
         checkDir = list(map(lambda x: x if(os.path.isdir(x) == True) else None, os.listdir(".")))
         for file in checkDir:
             if file != None and (os.path.abspath(file) in self.visitedFolders) == False:
-                self.beBack.append(file)
+                beBack.append(file)
             else:
                 pass
-        return self.beBack
+        return beBack
     def emptyFilesFinder(self):
+        def findType():
+            inside = ()
+            info = list(map(lambda x: "dir" if(os.path.isdir(x) == True) else "file", os.listdir(".")))
+            for i in range(len(info)):
+                if (os.path.abspath(os.listdir(".")[i]) in self.visitedFolders) != True:
+                    inside += [info[i], os.listdir(".")[i]],
+                else:
+                    pass
+            return inside
+        self.mainFolder = findType()
+        #self.emptyFiles = open("emptyFiles.txt", "a+")
+        #self.emptyFiles.write("Hello, there are locacions of empty files, have a nice day!\n")
+        self.routingFile = None
+        for number in self.mainFolder:
+            if number[0] == "dir":
+                i = 0
+                os.chdir(number[1])
+                while i != 1:
+                    if os.getcwd() == self.homeDir:
+                        self.visitedFolders = list()
+                        i = 1
+                    else:
+                        self.remoteCloud = findType()
+                        if len(self.remoteCloud) == 0:
+                            self.visitedFolders += os.getcwd(),
+                            os.chdir("..")
+                        else:
+                            if self.remoteCloud[0][0] == "file":
+                                self.routingFile = os.stat(self.remoteCloud[0][1]).st_size
+                                if self.routingFile == 0:
+                                    print("Oh an empty file")
+                                    #self.emptyFiles.write(os.path.abspath(self.remoteCloud[0][1])+"\n")
+                                else:
+                                    pass
+                            else:
+                                os.chdir(self.remoteCloud[0][1])
+                            self.visitedFolders += os.path.abspath(self.remoteCloud[0][1]),
+            else:
+                self.routingFile = os.stat(number[1]).st_size
+                if self.routingFile == 0:
+                    print("Oh an empty file!")
+                    #self.emptyFiles.write(os.path.abspath(number[1]))
+                else:
+                    pass
+        #self.emptyFiles.close()
+        print(str((time.time()-self.timeProgram)/60))
+    def emptyDirsFinder(self):
+        self.emptyDirs = open("emptyDirs.txt", "a+")
+        self.mainFolder = self.findDir()
         print("I'm working on it..")
         for main in self.mainFolder:
-            self.i = 0
+            i = 0
             self.visitedFolders += os.path.abspath(main),
             #self.visitedFolders.append(os.path.abspath(main))
             os.chdir(main)
-            while self.i != 1:
+            while i != 1:
                 if os.path.abspath(".") == self.homeDir:
-                    self.i = 1
+                    i = 1
                 else:
-                    self.remoteDir = self.findDir()
-                    del self.beBack
+                    self.remoteCloud = self.findDir()
                     if len(os.listdir(".")) == 0:
                         self.emptyDirs.write(os.getcwd()+"\n")
                         os.chdir("..")
-                    elif len(self.remoteDir) == 0:
+                    elif len(self.remoteCloud) == 0:
                         os.chdir("..")
                     else:
-                        self.visitedFolders += os.path.abspath(self.remoteDir[0]),
-                        #self.visitedFolders.append(os.path.abspath(self.remoteDir[0]))
-                        os.chdir(self.remoteDir[0])
+                        self.visitedFolders += os.path.abspath(self.remoteCloud[0]),
+                        #self.visitedFolders.append(os.path.abspath(self.remoteCloud[0]))
+                        os.chdir(self.remoteCloud[0])
         self.endMe()
     def endMe(self):
         self.emptyDirs.close()
